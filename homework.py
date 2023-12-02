40c54dffe0537f56e0aa9a7bb8940b58e7ad4986
Стас Баринов <stanislavbarinov0@gmail.com>
2023-12-02 19:50:10 +0300

    0.1.5

--- homework.py
+++ homework.py
@@ -10,8 +10,8 @@ class InfoMessage:
 
     def get_message(self) -> str:
         return (f'Тип тренировки: {self.training_type}; '
-                f'Длительность: {self.duration} ч.; '
-                f'Дистанция: {self.distance} км; '
+                f'Длительность: {self.duration:.3f} ч.; '
+                f'Дистанция: {self.distance:.3f} км; '
                 f'Ср. скорость: {self.speed:.3f} км/ч; '
                 f'Потрачено ккал: {self.calories:.3f}.')
 
